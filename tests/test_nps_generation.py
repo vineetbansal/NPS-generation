3e9650866734297737560082c8ff9048eb4de885
anushka255 <aachary2@ramapo.edu>
2023-10-17 00:21:53 -0400

    Integrated test case for augment-SMILES

--- tests/test_nps_generation.py
+++ tests/test_nps_generation.py
@@ -1,10 +1,21 @@
 import os
-from NPS_generation.clean_SMILES import main
+from NPS_generation.clean_SMILES import main as clean_SMILES_main
+from NPS_generation.augment_SMILES import main as augment_SMILES_main
 import NPS_generation.data as data_folder
+
+test_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = data_folder.__path__[0]
 
 
 def test_clean_SMILES():
-    main_dir = data_folder.__path__
-    input_file = os.path.join(main_dir[0], "chembl_28_2000.smi")
-    main(input_file, output_file="output1.smi")
+    input_file = os.path.join(data_dir, "chembl_28_2000.smi")
+    output_file = os.path.join(test_dir, "test_data", "output1.smi")
+    clean_SMILES_main(input_file, output_file)
+    assert True
+
+
+def test_augment_SMILES():
+    input_file = os.path.join(test_dir, "test_data", "output1.smi")
+    output_file = os.path.join(test_dir, "test_data", "output2.smi")
+    augment_SMILES_main(input_file, output_file, enum_factor=1)
     assert True
