48fb5ce2b282589621019e21b3da9d4dca882549
Vineet Bansal <vineetbansal@protonmail.com>
2024-02-13 15:58:36 -0500

    deterministic snakemake runs

--- tests/test_snakemake.py
+++ tests/test_snakemake.py
@@ -3,6 +3,7 @@ import os.path
 from pathlib import Path
 import snakemake
 import tempfile
+import hashlib
 
 
 base_dir = Path(__file__).parent.parent
@@ -19,7 +20,7 @@ def test_snakemake():
             snakefile=snakefile,
             cores=1,
             configfiles=[config_file],
-            config={"dataset": dataset, "pubchem_tsv_file": pubchem_tsv_file, "output_dir": temp_dir},
+            config={"random_seed": 5831, "dataset": dataset, "pubchem_tsv_file": pubchem_tsv_file, "output_dir": temp_dir},
             dryrun=False,
             latency_wait=60,
             forceall=True,
@@ -27,3 +28,11 @@ def test_snakemake():
             verbose=True
         )
         assert success, "Snakemake did not complete successfully"
+
+        ranks_file_overall = f"{temp_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_ranks_structure.csv"
+        checksum = hashlib.md5(''.join(sorted(open(ranks_file_overall, "r").readlines())).encode('utf8')).hexdigest()
+        assert checksum == 'eaea0450e4f94cd7862d231164dde179'
+
+        tc_file_overall = f"{temp_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_tc.csv"
+        checksum = hashlib.md5(''.join(sorted(open(tc_file_overall, "r").readlines())).encode('utf8')).hexdigest()
+        assert checksum == '8d02849171f5b26ed3e90ff3cdc50d6e'
