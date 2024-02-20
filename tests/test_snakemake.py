17e994509bd3c00a9bcf4710b5e67119df257d6f
Vineet Bansal <vineetbansal@protonmail.com>
2024-02-20 15:57:45 -0500

    saving artifacts for inspection

--- tests/test_snakemake.py
+++ tests/test_snakemake.py
@@ -2,7 +2,6 @@ import os
 import os.path
 from pathlib import Path
 import snakemake
-import tempfile
 import hashlib
 
 
@@ -15,33 +14,33 @@ pubchem_tsv_file = base_dir / "tests/test_data/PubChem_truncated.tsv"
 
 
 def test_snakemake():
-    with tempfile.TemporaryDirectory() as temp_dir:
-        success = snakemake.snakemake(
-            snakefile=snakefile,
-            cores=1,
-            configfiles=[config_file],
-            config={
-                "random_seed": 5831,
-                "dataset": dataset,
-                "pubchem_tsv_file": pubchem_tsv_file,
-                "output_dir": temp_dir,
-            },
-            dryrun=False,
-            latency_wait=60,
-            forceall=True,
-            verbose=True,
-        )
-        assert success, "Snakemake did not complete successfully"
+    out_dir = "out"
+    success = snakemake.snakemake(
+        snakefile=snakefile,
+        cores=1,
+        configfiles=[config_file],
+        config={
+            "random_seed": 5831,
+            "dataset": dataset,
+            "pubchem_tsv_file": pubchem_tsv_file,
+            "output_dir": "out",
+        },
+        dryrun=False,
+        latency_wait=60,
+        forceall=True,
+        verbose=True,
+    )
+    assert success, "Snakemake did not complete successfully"
 
-        output_dir = os.path.join(os.path.join(snakefile), temp_dir)
-        ranks_file_overall = f"{output_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_ranks_structure.csv"
-        checksum = hashlib.md5(
-            "".join(open(ranks_file_overall, "r").readlines()).encode("utf8")
-        ).hexdigest()
-        assert checksum == "eb1b8299b54fc36eef4f067ac0819d7d"
+    output_dir = os.path.join(os.path.join(snakefile), out_dir)
+    ranks_file_overall = f"{output_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_ranks_structure.csv"
+    checksum = hashlib.md5(
+        "".join(open(ranks_file_overall, "r").readlines()).encode("utf8")
+    ).hexdigest()
+    assert checksum == "eb1b8299b54fc36eef4f067ac0819d7d"
 
-        tc_file_overall = f"{output_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_tc.csv"
-        checksum = hashlib.md5(
-            "".join(open(tc_file_overall, "r").readlines()).encode("utf8")
-        ).hexdigest()
-        assert checksum == "c6e24fa270b159239835f83ace71ff1f"
+    tc_file_overall = f"{output_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_tc.csv"
+    checksum = hashlib.md5(
+        "".join(open(tc_file_overall, "r").readlines()).encode("utf8")
+    ).hexdigest()
+    assert checksum == "c6e24fa270b159239835f83ace71ff1f"
