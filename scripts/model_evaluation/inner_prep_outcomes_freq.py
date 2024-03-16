56a8be591315a2e45a47ff0b4acef087b885c134
anushka255 <aa9078@princeton.edu>
2024-03-16 03:32:27 -0400

    updated ways to save frequency bin

--- scripts/model_evaluation/inner_prep_outcomes_freq.py
+++ scripts/model_evaluation/inner_prep_outcomes_freq.py
@@ -3,49 +3,53 @@ import os
 import pandas as pd
 import re
 
-parser = argparse.ArgumentParser(description=__doc__)
 
-parser.add_argument("--sample_file", type=str, help="Path to the sampled file")
-parser.add_argument("--sample_no", type=int, help="Number of samples to select")
-parser.add_argument("--pubchem_file", type=str, help="Path to the PubChem file")
-parser.add_argument("--output_file", type=str, help="Path to the save the output file")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
 
-args = parser.parse_args()
-sample_file = args.sample_file
-sample_no = args.sample_no
-pubchem_file = args.pubchem_file
-output_file = args.output_file
+    parser.add_argument("--sample_file", type=str, help="Path to the sampled file")
+    parser.add_argument("--sample_no", type=int, help="Number of samples to select")
+    parser.add_argument("--pubchem_file", type=str, help="Path to the PubChem file")
+    parser.add_argument("--output_file", type=str, help="Path to the save the output file")
 
-data = pd.read_csv(sample_file)
+    args = parser.parse_args()
+    sample_file = args.sample_file
+    sample_no = args.sample_no
+    pubchem_file = args.pubchem_file
+    output_file = args.output_file
 
-# TODO: make this process dynamic later
-frequency_ranges = [1, 2, (3, 10), (11, 30), (31, 100), '>100']
+    data = pd.read_csv(sample_file)
 
-df = pd.DataFrame()
-for f_range in frequency_ranges:
-    match f_range:
-        case int(x):
-            sample = data[data['size'] == x]
+    # TODO: make this process dynamic later
+    frequency_ranges = [1, 2, (3, 10), (11, 30), (31, 100), '>100']
 
-        case (int(x), int(y)):
-            sample = data[data['size'].between(x, y)]
+    dfs = []
+    for f_range in frequency_ranges:
+        match f_range:
+            case int(x):
+                sample = data[data['size'] == x]
 
-        case _ if re.search(r'>(\d+)', f_range):
-            x = int(re.findall(r'>(\d+)', f_range)[0])
-            sample = data[data['size'] > x]
+            case (int(x), int(y)):
+                sample = data[data['size'].between(x, y)]
 
-        case _ if re.search(r'<(\d+)', f_range):
-            x = int(re.findall(r'<(\d+)', f_range)[0])
-            sample = data[data['size'] > x]
+            case _ if re.search(r'>(\d+)', f_range):
+                x = int(re.findall(r'>(\d+)', f_range)[0])
+                sample = data[data['size'] > x]
 
-        case _:
-            sample = pd.DataFrame()
+            case _ if re.search(r'<(\d+)', f_range):
+                x = int(re.findall(r'<(\d+)', f_range)[0])
+                sample = data[data['size'] > x]
 
-    if sample.shape[0] >= sample_no:
-        sample = sample.sample(n=sample_no, ignore_index=True).assign(bin=str(f_range))
+            case _:
+                sample = pd.DataFrame()
 
-    df = pd.concat([df, sample])
+        if sample.shape[0] >= sample_no:
+            sample = sample.sample(n=sample_no, ignore_index=True).assign(bin=str(f_range))
 
-df.to_csv(output_file)
+        dfs.append((sample, f_range))
+
+    for df in dfs:
+        output = f"{os.path.dirname(output_file)}/freq_{str(df[1])}_{os.path.basename(output_file)}"
+        df[0].to_csv(output)
 
 
