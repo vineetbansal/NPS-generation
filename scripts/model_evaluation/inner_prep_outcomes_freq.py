06ad7ce1db92d01b6c33d286816f434dd8aca027
anushka255 <aa9078@princeton.edu>
2024-03-18 10:29:50 -0400

    some cleanups:

--- scripts/model_evaluation/inner_prep_outcomes_freq.py
+++ scripts/model_evaluation/inner_prep_outcomes_freq.py
@@ -4,22 +4,18 @@ import pandas as pd
 import re
 
 
-if __name__ == "__main__":
+def add_args(parser):
     parser = argparse.ArgumentParser(description=__doc__)
 
     parser.add_argument("--sample_file", type=str, help="Path to the sampled file")
-    parser.add_argument("--sample_no", type=int, help="Number of samples to select")
-    parser.add_argument("--pubchem_file", type=str, help="Path to the PubChem file")
+    parser.add_argument("--max_molecules", type=int, help="Number of samples to select")
     parser.add_argument(
         "--output_file", type=str, help="Path to the save the output file"
     )
+    return parser
 
-    args = parser.parse_args()
-    sample_file = args.sample_file
-    sample_no = args.sample_no
-    pubchem_file = args.pubchem_file
-    output_file = args.output_file
 
+def prep_outcomes_freq(sample_file, max_molecules, output_file):
     data = pd.read_csv(sample_file)
 
     # TODO: make this process dynamic later
@@ -45,8 +41,8 @@ if __name__ == "__main__":
             case _:
                 sample = pd.DataFrame()
 
-        if sample.shape[0] >= sample_no:
-            sample = sample.sample(n=sample_no, ignore_index=True).assign(
+        if sample.shape[0] >= max_molecules:
+            sample = sample.sample(n=max_molecules, ignore_index=True).assign(
                 bin=str(f_range)
             )
 
@@ -55,3 +51,17 @@ if __name__ == "__main__":
     for df in dfs:
         output = f"{os.path.dirname(output_file)}/freq_{str(df[1])}_{os.path.basename(output_file)}"
         df[0].to_csv(output)
+
+
+def main(args):
+    prep_outcomes_freq(
+        sample_file=args.sample_file,
+        max_molecules=args.max_molecules,
+        output_file=args.output_file,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    args = add_args(parser).parse_args()
+    main(args)
