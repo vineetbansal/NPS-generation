1d5d9d73ca0e6c3a5959351799b3f1f19a18f2cc
anushka255 <aa9078@princeton.edu>
2024-03-19 10:52:40 -0400

    passing precommit checks

--- tests/test_model_evaluation.py
+++ tests/test_model_evaluation.py
@@ -76,9 +76,9 @@ def test_train_discriminator():
         output_file = Path(temp_dir) / "train_discriminator.csv"
         outcomes = train_discriminator(
             train_file=test_dir
-                       / "snakemake_output/0/prior/inputs/train_LOTUS_truncated_SMILES_all.smi",
+            / "snakemake_output/0/prior/inputs/train_LOTUS_truncated_SMILES_all.smi",
             sample_file=test_dir
-                        / "snakemake_output/0/prior/samples/LOTUS_truncated_SMILES_processed_freq-avg.csv",
+            / "snakemake_output/0/prior/samples/LOTUS_truncated_SMILES_processed_freq-avg.csv",
             output_file=output_file,
             seed=0,
         )
