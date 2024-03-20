602c03e32ff5bb8e82e9698efcb86a2cefcb203d
anushka255 <66816733+anushka255@users.noreply.github.com>
2024-03-20 15:18:12 -0400

    added write_frequency_distribution along with its test case (#75)
    * added write_frequency_distribution along with its test case

--- tests/test_model_evaluation.py
+++ tests/test_model_evaluation.py
@@ -10,6 +10,7 @@ from clm.commands.calculate_outcomes import (
 )
 from clm.commands.write_nn_Tc import write_nn_Tc
 from clm.commands.train_discriminator import train_discriminator
+from clm.commands.write_freq_distribution import write_freq_distribution
 
 base_dir = Path(__file__).parent.parent
 test_dir = base_dir / "tests/test_data"
@@ -68,6 +69,19 @@ def test_write_nn_tc():
         )
 
         true_outcomes = pd.read_csv(test_dir / "write_nn_tc.csv")
+        pd.testing.assert_frame_equal(outcomes, true_outcomes)
+
+
+def test_write_freq_distribution():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        output_file = Path(temp_dir) / "write_freq_distribution.csv"
+        outcomes = write_freq_distribution(
+            sampled_file=test_dir / "LOTUS_SMILES_processed_freq-avg_trunc.csv",
+            test_file=test_dir / "test_LOTUS_SMILES_all_trunc.smi",
+            output_file=output_file,
+        )
+
+        true_outcomes = pd.read_csv(test_dir / "write_freq_distribution.csv")
         pd.testing.assert_frame_equal(outcomes, true_outcomes)
 
 
