73928b945b67d92980a6eda3d19532d6c4712e56
Vineet Bansal <vineetbansal@protonmail.com>
2024-03-27 11:08:40 -0700

    refactored calculate_outcomes to use DataFrames (easier to reason about) (#84)

--- tests/test_model_evaluation.py
+++ tests/test_model_evaluation.py
@@ -1,11 +1,10 @@
 from pathlib import Path
 import tempfile
 import pandas as pd
-import os.path
 import pytest
 from clm.commands.calculate_outcomes import (
-    get_dicts,
-    process_outcomes,
+    get_dataframes,
+    calculate_outcomes_dataframe,
     calculate_outcomes,
 )
 from clm.commands.write_nn_Tc import write_nn_Tc
@@ -17,30 +16,24 @@ base_dir = Path(__file__).parent.parent
 test_dir = base_dir / "tests/test_data"
 
 
-@pytest.mark.xfail
+@pytest.mark.xfail(strict=True)
 def test_generate_outcome_dicts():
-    train_dict, gen_dict = get_dicts(
+    train_df, sample_df = get_dataframes(
         train_file=test_dir / "prep_outcomes_freq.csv",
         sampled_file=test_dir / "LOTUS_SMILES_processed_freq-avg_trunc.csv",
         max_orig_mols=10000,
-        seed=12,
     )
 
     # Certain fields in train_dict/gen_dict need to be tolerant of Nones
-    train_dict["qed"][4] = None
-    gen_dict["SA"][3] = None
+    train_df["qed"].iloc[4] = None
+    sample_df["SA"].iloc[3] = None
 
-    process_outcomes(
-        train_dict,
-        gen_dict,
-        "out.txt",
-        test_dir / "LOTUS_SMILES_processed_freq-avg_trunc.csv",
+    calculate_outcomes_dataframe(
+        train_df,
+        sample_df,
     )
 
 
-# TODO: the train file in this context should be bound to the sampled_file variable, and the actual train file
-# TODO contd.: should be the train file generated at the beginning of the workflow, the confusion was initially
-#  TODO contd.: caused by the use of csv file in place of train_file
 def test_calculate_outcomes():
     with tempfile.TemporaryDirectory() as temp_dir:
         output_file = Path(temp_dir) / "calculate_outcome.csv"
@@ -52,18 +45,18 @@ def test_calculate_outcomes():
             max_orig_mols=10000,
             seed=12,
         )
-        # ignore leading folders of the filename
-        outcomes["input_file"] = outcomes["input_file"].apply(
-            lambda path: os.path.basename(path)
-        )
 
         true_outcomes = pd.read_csv(
             test_dir / "calculate_outcome.csv", keep_default_na=False
         )
         # https://stackoverflow.com/questions/14224172
         pd.testing.assert_frame_equal(
-            outcomes.sort_index(axis=1).reset_index(drop=True),
-            true_outcomes.sort_index(axis=1).reset_index(drop=True),
+            outcomes.sort_index(axis=1)
+            .sort_values(["outcome", "bin"])
+            .reset_index(drop=True),
+            true_outcomes.sort_index(axis=1)
+            .sort_values(["outcome", "bin"])
+            .reset_index(drop=True),
         )
 
 
