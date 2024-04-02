e1919a44ab7a25de9553c1d6fbba6e2da02ed27a
anushka255 <aa9078@princeton.edu>
2024-04-02 16:32:06 -0400

    some updates

--- scripts/figures/outcome_distr.py
+++ scripts/figures/outcome_distr.py
@@ -9,18 +9,12 @@ import seaborn as sns
 def add_args(parser):
     parser.add_argument("--outcome_dir", type=str, help="Path to outcome directory")
     parser.add_argument(
-        "--outcome_type", type=str, help="The metrics that you want to plot"
+        "--plot_type", type=str, help="The metrics that you want to plot"
     )
     return parser
 
 
-def plot(outcome_dir, outcome_type):
-    outcome_map = {
-        "Molecular weight",
-        "QED",
-        "% sp3 carbons"
-    }
-
+def plot(outcome_dir, plot_type):
     outcome_files = glob.glob(f"{outcome_dir}/*calculate_outcome_distrs.csv")
     outcome = pd.concat(
         [pd.read_csv(outcome_file, delimiter=",") for outcome_file in outcome_files]
@@ -28,27 +22,34 @@ def plot(outcome_dir, outcome_type):
 
     split_outcomes = {outcome: df for outcome, df in outcome.groupby("outcome")}
 
-    for outcome_type in outcome_map:
-        chosen_outcome = split_outcomes[outcome_type]
-        split_source = pd.DataFrame({
-            source: df['value'].reset_index(drop=True) for source, df in chosen_outcome.groupby('source')
-        })
+    if plot_type == 'A':
+        outcome_map = {
+            "Molecular weight",
+            "QED",
+            "% sp3 carbons"
+        }
+        for plot_type in outcome_map:
+            chosen_outcome = split_outcomes[plot_type]
+            split_source = pd.DataFrame({
+                source: df['value'].reset_index(drop=True) for source, df in chosen_outcome.groupby('source')
+            })
 
-        sns.kdeplot(
-            data=split_source,
-            palette=sns.color_palette("pastel"),
-        )
+            sns.kdeplot(
+                data=split_source,
+                palette=sns.color_palette("pastel"),
+            )
 
-        plt.title(outcome_type)
-        plt.ylabel("Density")
-        plt.xlabel(outcome_type)
-        file_path = f"scripts/figures/ped_fig/{outcome_type}.png"
-        plt.savefig(file_path)
-        plt.show()
+            plt.title(plot_type)
+            plt.ylabel("Density")
+            plt.xlabel(plot_type)
+            file_path = f"scripts/figures/ped_fig/{plot_type}.png"
+            plt.savefig(file_path)
+            plt.show()
+
 
 
 def main(args):
-    plot(outcome_dir=args.outcome_dir, outcome_type=args.outcome_type)
+    plot(outcome_dir=args.outcome_dir, plot_type=args.plot_type)
 
 
 if __name__ == "__main__":
