dab3a7c70dd4702f2b9b3269f1fdc4ebe47ec165
Vineet Bansal <vineetbansal@protonmail.com>
2024-04-04 13:56:27 -0700

    plotting outcomes in relevant test (#103)

--- src/clm/plot/calculate_outcomes.py
+++ src/clm/plot/calculate_outcomes.py
@@ -27,67 +27,18 @@ def plot(outcome_dir, output_dir):
     # Make output directory if it doesn't exist yet
     os.makedirs(output_dir, exist_ok=True)
 
-    freq_map = {
-        "1-1": "1",
-        "2-2": "2",
-        "3-10": "3-10",
-        "11-30": "11-30",
-        "31-100": "31-100",
-        "101-": ">100",
-    }
-    plot_types = {
-        "% novel",
-        "% unique",
-        "% valid",
-        "External diversity",
-        "External nearest-neighbor Tc",
-        "Frechet ChemNet distance",
-        "Internal diversity",
-        "Internal nearest-neighbor Tc",
-        "Jensen-Shannon distance, # of aliphatic rings",
-        "Jensen-Shannon distance, # of aromatic rings",
-        "Jensen-Shannon distance, # of rings",
-        "Jensen-Shannon distance, % rotatable bonds",
-        "Jensen-Shannon distance, % sp3 carbons",
-        "Jensen-Shannon distance, % stereocenters",
-        "Jensen-Shannon distance, Bertz TC",
-        "Jensen-Shannon distance, MWs",
-        "Jensen-Shannon distance, Murcko scaffolds",
-        "Jensen-Shannon distance, NP score",
-        "Jensen-Shannon distance, QED",
-        "Jensen-Shannon distance, SA score",
-        "Jensen-Shannon distance, TPSA",
-        "Jensen-Shannon distance, atoms",
-        "Jensen-Shannon distance, hydrogen acceptors",
-        "Jensen-Shannon distance, hydrogen donors",
-        "Jensen-Shannon distance, logP",
-        "KL divergence, atoms",
-        "Wasserstein distance, atoms",
-        "n_mols",
-    }
-
     outcome_files = glob.glob(f"{outcome_dir}/*calculate_outcomes.csv")
     outcome = pd.concat(
         [pd.read_csv(outcome_file, delimiter=",") for outcome_file in outcome_files]
     )
 
-    # Split the data frame by outcomes and extract the one chosen to be plotted
-    split_outcomes = {df["outcome"].iloc[0]: df for _, df in outcome.groupby("outcome")}
-
     # Plot every figure possible
-    for plot_type in plot_types:
-        chosen_outcome = split_outcomes[plot_type]
-
-        # Generate a dictionary of frequency bin and respective value to be plotted
-        split_freq = {
-            freq_map[df["bin"].iloc[0]]: list(df["value"])
-            for df in [
-                chosen_outcome[chosen_outcome["bin"] == i] for i in freq_map.keys()
-            ]
-        }
-
-        data = list(split_freq.values())
-        labels = list(split_freq.keys())
+    for outcome_name, _outcome in outcome.groupby("outcome"):
+        data = []
+        labels = []
+        for bin, df in _outcome.groupby("bin"):
+            data.append(df["value"].tolist())
+            labels.append(bin)
 
         sns.violinplot(
             data=data,
@@ -99,12 +50,12 @@ def plot(outcome_dir, output_dir):
             native_scale=True,
         )
 
-        plt.title(plot_type)
-        plt.ylabel(plot_type)
+        plt.title(outcome_name)
+        plt.ylabel(outcome_name)
         plt.xlabel("Frequency")
         plt.xticks(list(range(len(labels))), labels)
 
-        file_name = Path(output_dir) / plot_type
+        file_name = Path(output_dir) / outcome_name
         plt.savefig(file_name)
 
         # Clear the previous figure
