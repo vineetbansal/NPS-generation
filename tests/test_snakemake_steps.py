b103a144e94a9d858061a874b90cd1d8b59c2031
anushka255 <66816733+anushka255@users.noreply.github.com>
2024-04-24 11:29:48 -0400

    Addressing some of the test cases mentioned in #Issue 131 (#143)
    * addressing some of the test cases mentioned in #issue131

--- tests/test_snakemake_steps.py
+++ tests/test_snakemake_steps.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import tempfile
 
+import pandas as pd
+
 from clm.commands import (
     preprocess,
     create_training_sets,
@@ -37,20 +39,23 @@ def test_00_preprocess():
 def test_01_create_training_sets():
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_dir = Path(temp_dir)
-        create_training_sets.create_training_sets(
-            input_file=test_dir / "prior/raw/LOTUS_truncated.txt",
-            train0_file=temp_dir / "train0_file_{fold}",
-            train_file=temp_dir / "train_file_{fold}",
-            vocab_file=temp_dir / "vocabulary_file_{fold}",
-            test0_file=temp_dir / "test0_file_{fold}",
-            test_file=temp_dir / "test_file_{fold}",
-            enum_factor=0,
-            folds=3,
-            representation="SMILES",
-            min_tc=0,
-            seed=5831,
-            max_input_smiles=1000,
-        )
+        folds = 3
+        for fold in range(folds):
+            create_training_sets.create_training_sets(
+                input_file=test_dir / "prior/raw/LOTUS_truncated.txt",
+                train0_file=temp_dir / "train0_file_{fold}",
+                train_file=temp_dir / "train_file_{fold}",
+                vocab_file=temp_dir / "vocabulary_file_{fold}",
+                test0_file=temp_dir / "test0_file_{fold}",
+                test_file=temp_dir / "test_file_{fold}",
+                enum_factor=0,
+                folds=folds,
+                which_fold=fold,
+                representation="SMILES",
+                min_tc=0,
+                seed=5831,
+                max_input_smiles=1000,
+            )
         # `train0_file_0` denotes the train smiles without augmentation for fold
         # 0; Since we're running with enum_factor=0, this should be identical
         # to `train_file_0` (train smiles with augmentation for fold 0)
@@ -71,6 +76,21 @@ def test_01_create_training_sets():
             temp_dir / "test_file_0",
             test_dir / "0/prior/inputs/test_LOTUS_truncated_SMILES_0.smi",
         )
+
+        # Check that the same InChI key does not appear in both the training and test set in any CV split
+        test0_all = []
+        for fold in range(folds):
+            train0 = pd.read_csv(temp_dir / f"train0_file_{fold}")
+            test0 = pd.read_csv(temp_dir / f"test0_file_{fold}")
+
+            train0_inchi, test0_inchi = set(train0.inchikey), set(test0.inchikey)
+            assert train0_inchi.isdisjoint(test0_inchi)
+
+            test0_all.append(test0)
+
+        # Check that there's no redundant InChI key in test sets across CV splits
+        test0_all = pd.concat(test0_all)
+        assert len(test0_all.inchikey) == len(test0_all.inchikey.unique())
 
 
 def test_02_train_models_RNN():
@@ -127,24 +147,38 @@ def test_03_sample_molecules_RNN():
 
 def test_04_tabulate_molecules():
     with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir) / "0/prior/samples"
+        train_file = test_dir / "0/prior/inputs/train_LOTUS_truncated_SMILES_0.smi"
+        output_file = (
+            Path(temp_dir)
+            / "0/prior/samples/LOTUS_truncated_SMILES_0_0_0_samples_masses.csv"
+        )
         inner_tabulate_molecules.tabulate_molecules(
             input_file=test_dir
             / "0/prior/samples/LOTUS_truncated_SMILES_0_0_0_samples.csv",
             representation="SMILES",
-            train_file=test_dir / "0/prior/inputs/train_LOTUS_truncated_SMILES_0.smi",
-            output_file=temp_dir / "LOTUS_truncated_SMILES_0_0_0_samples_masses.csv",
+            train_file=train_file,
+            output_file=output_file,
         )
         assert_checksum_equals(
-            temp_dir / "LOTUS_truncated_SMILES_0_0_0_samples_masses.csv",
+            output_file,
             test_dir
             / "0/prior/samples/LOTUS_truncated_SMILES_0_0_0_samples_masses.csv",
         )
+        train = pd.read_csv(train_file)
+        result = pd.read_csv(output_file)
+
+        # Check that same InChI key does not appear in more than one row
+        assert len(result.inchikey) == len(result.inchikey.unique())
+        # None of the InChI keys in the tabulated molecules should be in the training set
+        assert set(train.inchikey).isdisjoint(set(result.inchikey))
 
 
 def test_05_collect_tabulated_molecules():
     with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir) / "0/prior/samples"
+        output_file = (
+            Path(temp_dir)
+            / "0/prior/samples/LOTUS_truncated_SMILES_0_unique_masses.csv"
+        )
         inner_collect_tabulated_molecules.collect_tabulated_molecules(
             input_files=[
                 test_dir
@@ -152,17 +186,25 @@ def test_05_collect_tabulated_molecules():
                 test_dir
                 / "0/prior/samples/LOTUS_truncated_SMILES_0_1_0_samples_masses.csv",
             ],
-            output_file=temp_dir / "LOTUS_truncated_SMILES_0_unique_masses.csv",
+            output_file=output_file,
         )
         assert_checksum_equals(
-            temp_dir / "LOTUS_truncated_SMILES_0_unique_masses.csv",
+            output_file,
             test_dir / "0/prior/samples/LOTUS_truncated_SMILES_0_unique_masses.csv",
         )
+
+        result = pd.read_csv(output_file)
+
+        # Check that same InChI key does not appear in more than one row
+        assert len(result.inchikey) == len(result.inchikey.unique())
 
 
 def test_06_process_tabulated_molecules():
     with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir) / "0/prior/samples"
+        output_file = (
+            Path(temp_dir)
+            / "0/prior/samples/LOTUS_truncated_SMILES_processed_freq-avg.csv"
+        )
         inner_process_tabulated_molecules.process_tabulated_molecules(
             input_file=[
                 test_dir / "0/prior/samples/LOTUS_truncated_SMILES_0_unique_masses.csv",
@@ -174,13 +216,17 @@ def test_06_process_tabulated_molecules():
                 test_dir / "0/prior/inputs/train_LOTUS_truncated_SMILES_1.smi",
                 test_dir / "0/prior/inputs/train_LOTUS_truncated_SMILES_2.smi",
             ],
-            output_file=temp_dir / "LOTUS_truncated_SMILES_processed_freq-avg.csv",
+            output_file=output_file,
             summary_fn="freq-avg",
         )
         assert_checksum_equals(
-            temp_dir / "LOTUS_truncated_SMILES_processed_freq-avg.csv",
+            output_file,
             test_dir / "0/prior/samples/LOTUS_truncated_SMILES_processed_freq-avg.csv",
         )
+
+        result = pd.read_csv(output_file)
+        # Check that same InChI key does not appear in more than one row
+        assert len(result.inchikey) == len(result.inchikey.unique())
 
 
 def test_07_write_structural_prior_CV():
