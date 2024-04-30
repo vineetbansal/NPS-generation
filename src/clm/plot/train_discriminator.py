0cecbd73e182b96cf5cd98f235b10ff1743a1f44
anushka255 <aa9078@princeton.edu>
2024-04-30 11:21:37 -0400

    minor tweaks

--- src/clm/plot/train_discriminator.py
+++ src/clm/plot/train_discriminator.py
@@ -86,7 +86,7 @@ def plot(outcome_dir, output_dir):
     # Make output directory if it doesn't exist yet
     os.makedirs(output_dir, exist_ok=True)
 
-    outcome_files = glob.glob(f"{outcome_dir}/*train_discriminator_.csv")
+    outcome_files = glob.glob(f"{outcome_dir}/*train_discriminator.csv")
     outcome = pd.concat(
         [pd.read_csv(outcome_file, delimiter=",") for outcome_file in outcome_files]
     )
