aeba7574068b136a7d79bd178fbbe2e1f03fae86
anushka255 <aa9078@princeton.edu>
2024-04-30 11:14:39 -0400

    introducing plot for nn_tc ever vs never generated

--- src/clm/commands/write_nn_Tc.py
+++ src/clm/commands/write_nn_Tc.py
@@ -31,7 +31,16 @@ def find_max_similarity_fingerprint(target_smile, ref_smiles, ref_fps):
     if target_fps is None:
         return None, None
 
-    tcs = [FingerprintSimilarity(target_fps, ref_fp) for ref_fp in ref_fps]
+    tcs = []
+    for ref_smile, ref_fp in zip(ref_smiles, ref_fps):
+        # Avoid comparing tcs of exactly same molecule
+        if not (
+            Chem.inchi.MolToInchiKey(clean_mol(target_smile, raise_error=False))
+            == Chem.inchi.MolToInchiKey(clean_mol(ref_smile, raise_error=False))
+        ):
+            tcs.append(FingerprintSimilarity(target_fps, ref_fp))
+        else:
+            tcs.append(-1)
 
     return np.max(tcs), ref_smiles[np.argmax(tcs)]
 
---
+++ src/clm/plot/nn_tc_ever_v_never.py
@@ -0,0 +1,87 @@
+import argparse
+import glob
+import pandas as pd
+from pathlib import Path
+import os
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+def add_args(parser):
+    parser.add_argument(
+        "--outcome_dir",
+        type=str,
+        help="Path to directory where all the model evaluation files are saved ",
+    )
+    parser.add_argument(
+        "--ranks_file",
+        type=str,
+        help="Path to ranks file ",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        help="Path to directory to save resulting images(s) at",
+    )
+    return parser
+
+
+def plot_generated_v_ref(outcome, output_dir):
+    data = []
+    # Fill all the empty ranks with 0
+    outcome["target_rank"].fillna(0, inplace=True)
+    data.append(list(outcome[~(outcome["target_rank"] == 0)]["nn_tc"]))
+    data.append(list(outcome[outcome["target_rank"] == 0]["nn_tc"]))
+
+    labels = ["Ever Generated", "Never Generated"]
+
+    sns.violinplot(
+        data=data,
+        palette=sns.color_palette("pastel"),
+        linecolor="auto",
+        width=0.2,
+        inner="box",
+        inner_kws={"box_width": 3},
+        native_scale=True,
+    )
+
+    plt.title(
+        "nearest-neighbor Tc for held-out molecules \n (ever generated vs. never generated)"
+    )
+    plt.ylabel("Nearest-neighbor Tc")
+    plt.xticks(list(range(len(labels))), labels)
+
+    file_name = Path(output_dir) / "nn_tc_ever_v_never"
+    plt.savefig(file_name)
+
+    # Clear the previous figure
+    plt.clf()
+
+
+def plot(outcome_dir, ranks_file, output_dir):
+    # Make an output directory if it doesn't yet
+    os.makedirs(output_dir, exist_ok=True)
+
+    outcome_files = glob.glob(f"{outcome_dir}/*_nn_tc_ever_v_never.csv")
+    outcome = pd.concat(
+        [pd.read_csv(outcome_file, delimiter=",") for outcome_file in outcome_files]
+    )
+    rank_df = pd.read_csv(ranks_file)
+    rank_df = rank_df[rank_df["target_source"] == "model"]
+
+    merged_df = pd.merge(outcome, rank_df, how="inner", on=["smiles"])
+    plot_generated_v_ref(merged_df, output_dir)
+
+
+def main(args):
+    plot(
+        outcome_dir=args.outcome_dir,
+        ranks_file=args.ranks_file,
+        output_dir=args.output_dir,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__)
+    args = add_args(parser).parse_args()
+    main(args)
