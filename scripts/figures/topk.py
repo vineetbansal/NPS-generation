5babc255612a851a8b11c0d29c32d12be3df0d7c
Vineet Bansal <vineetbansal@protonmail.com>
2024-04-30 12:27:16 -0400

    changes based on discussion on issue 140

--- scripts/figures/topk.py
+++ scripts/figures/topk.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     print(df.columns)
     print(df.shape)
 
-    models = ("model", "PubChem")
+    models = ("model", "PubChem", "train")
     ys = {model: [] for model in models}
 
     ks = range(0, 30)
