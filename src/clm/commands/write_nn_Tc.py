d340e418a26cdc203dd800bce2b9378b7569e443
anushka255 <aa9078@princeton.edu>
2024-05-02 13:46:38 -0400

    calculating inchikey from a file

--- src/clm/commands/write_nn_Tc.py
+++ src/clm/commands/write_nn_Tc.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from rdkit import Chem
 from rdkit.DataStructs import FingerprintSimilarity
-from clm.functions import clean_mol, read_file
+from clm.functions import clean_mol
 import os
 import logging
 
@@ -25,19 +25,20 @@ def calculate_fingerprint(smile):
     return None
 
 
-def find_max_similarity_fingerprint(target_smile, ref_smiles, ref_fps):
-    target_fps = calculate_fingerprint(target_smile)
+def find_max_similarity_fingerprint(target_row, ref_smiles, ref_fps, ref_inchikeys):
+    target_fps = calculate_fingerprint(target_row.smiles)
+    try:
+        target_inchikey = target_row.inchikey
+    except AttributeError:
+        target_inchikey = target_row.target_inchikey
 
     if target_fps is None:
         return None, None
 
     tcs = []
-    for ref_smile, ref_fp in zip(ref_smiles, ref_fps):
+    for ref_smile, ref_fp, ref_inchikey in zip(ref_smiles, ref_fps, ref_inchikeys):
         # Avoid comparing tcs of exactly same molecule
-        if not (
-            Chem.inchi.MolToInchiKey(clean_mol(target_smile, raise_error=False))
-            == Chem.inchi.MolToInchiKey(clean_mol(ref_smile, raise_error=False))
-        ):
+        if not (target_inchikey == ref_inchikey):
             tcs.append(FingerprintSimilarity(target_fps, ref_fp))
         else:
             tcs.append(-1)
@@ -46,17 +47,24 @@ def find_max_similarity_fingerprint(target_smile, ref_smiles, ref_fps):
 
 
 def write_nn_Tc(query_file, reference_file, output_file):
-    ref_fps, ref_smiles = [], []
-    for smile in read_file(reference_file, stream=True, smile_only=True):
-        if (fps := calculate_fingerprint(smile)) is not None:
+    ref_fps, ref_smiles, ref_inchikeys = [], [], []
+    for row in pd.read_csv(reference_file, chunksize=1, iterator=True):
+        if (fps := calculate_fingerprint(row.smiles.values[0])) is not None:
             ref_fps.append(fps)
-            ref_smiles.append(smile)
+            ref_smiles.append(row.smiles.values[0])
+            try:
+                ref_inchikeys.append(row.inchikey.values[0])
+            except AttributeError:
+                ref_inchikeys.append(row.target_inchikey.values[0])
 
     total_lines = sum(1 for _ in open(query_file, "r"))
     n_processed = 0
     for query in pd.read_csv(query_file, chunksize=10000):
-        results = query["smiles"].apply(
-            lambda x: find_max_similarity_fingerprint(x, ref_smiles, ref_fps)
+        results = query.apply(
+            lambda x: find_max_similarity_fingerprint(
+                x, ref_smiles, ref_fps, ref_inchikeys
+            ),
+            axis=1,
         )
         query = query.assign(nn_tc=[i[0] for i in results])
         query = query.assign(nn=[i[1] for i in results])
