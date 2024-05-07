dc2896da64f4d5677d1c904053e7e4b551e08a42
Vineet Bansal <vineetbansal@protonmail.com>
2024-05-06 20:23:50 -0400

    support for gz files throughout the pipeline; more tests

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import gzip
+import pytest
+
+
+base_dir = Path(__file__).parent.parent
+dataset_path = base_dir / "tests/test_data/LOTUS_truncated.txt"
+
+
+@pytest.fixture(scope="session")
+def dataset():
+    return dataset_path
+
+
+@pytest.fixture(scope="session")
+def dataset_compressed():
+    path = dataset_path.with_suffix(".txt.gz")
+    with gzip.open(path, "wb") as g:
+        with open(dataset_path, "rb") as f:
+            g.write(f.read())
+    yield path
--- tests/test_snakemake.py
+++ tests/test_snakemake.py
@@ -4,6 +4,9 @@ from pathlib import Path
 import snakemake
 import tempfile
 import hashlib
+import logging
+import gzip
+import clm  # noqa: F401 (imported to enable logging configuration)
 
 
 base_dir = Path(__file__).parent.parent
@@ -13,39 +16,53 @@ config_file = base_dir / "snakemake/config_fast.json"
 dataset = base_dir / "tests/test_data/LOTUS_truncated.txt"
 pubchem_tsv_file = base_dir / "tests/test_data/PubChem_truncated.tsv"
 
+logger = logging.getLogger("clm")
+
 
 def test_snakemake():
-    with tempfile.TemporaryDirectory() as temp_dir:
-        success = snakemake.snakemake(
-            snakefile=str(snakefile),
-            cores=1,
-            configfiles=[config_file],
-            config={
-                "random_seed": 5831,
-                "dataset": dataset,
-                "pubchem_tsv_file": pubchem_tsv_file,
-                "representations": ["SMILES"],
-                "enum_factors": [0],
-                "folds": 3,
-                "sample_seeds": [0],
-                "output_dir": temp_dir,
-            },
-            dryrun=False,
-            latency_wait=60,
-            forceall=True,
-            verbose=True,
-        )
-        assert success, "Snakemake did not complete successfully"
+    temp_dir = tempfile.mkdtemp()
+    logger.info(f"Running snakemake workflow in {temp_dir}")
+    success = snakemake.snakemake(
+        snakefile=str(snakefile),
+        cores=1,
+        configfiles=[config_file],
+        config={
+            "random_seed": 5831,
+            "dataset": dataset,
+            "pubchem_tsv_file": pubchem_tsv_file,
+            "representations": ["SMILES"],
+            "enum_factors": [0],
+            "folds": 3,
+            "sample_seeds": [0],
+            "output_dir": temp_dir,
+        },
+        dryrun=False,
+        latency_wait=60,
+        forceall=True,
+        verbose=True,
+        printshellcmds=True,
+    )
+    assert success, "Snakemake did not complete successfully"
 
-        output_dir = os.path.join(os.path.join(snakefile), temp_dir)
-        ranks_file_overall = f"{output_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_ranks_structure.csv"
-        checksum = hashlib.md5(
-            "".join(open(ranks_file_overall, "r").readlines()).encode("utf8")
-        ).hexdigest()
-        assert checksum == "09a6376e5427ab5ffe23b1a432a42c0f"
+    output_dir = os.path.join(os.path.join(snakefile), temp_dir)
+    ranks_file_overall = f"{output_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_ranks_structure.csv.gz"
+    checksum = hashlib.md5(
+        "".join(
+            [
+                line.decode("utf8")
+                for line in gzip.open(ranks_file_overall, "r").readlines()
+            ]
+        ).encode("utf8")
+    ).hexdigest()
+    assert checksum == "09a6376e5427ab5ffe23b1a432a42c0f"
 
-        tc_file_overall = f"{output_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_tc.csv"
-        checksum = hashlib.md5(
-            "".join(open(tc_file_overall, "r").readlines()).encode("utf8")
-        ).hexdigest()
-        assert checksum == "f70b7d22ce8110c50d832d431c4693d7"
+    tc_file_overall = f"{output_dir}/0/prior/structural_prior/LOTUS_truncated_SMILES_all_freq-avg_CV_tc.csv.gz"
+    checksum = hashlib.md5(
+        "".join(
+            [
+                line.decode("utf8")
+                for line in gzip.open(tc_file_overall, "r").readlines()
+            ]
+        ).encode("utf8")
+    ).hexdigest()
+    assert checksum == "f70b7d22ce8110c50d832d431c4693d7"
