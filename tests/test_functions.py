b476c4939fa4885e3f28353bc4eb50f16dee9175
Vineet Bansal <vineetbansal@protonmail.com>
2024-05-16 15:08:32 -0400

    tmp_path fixture in tests; fixed tiny bug in the process (#170)

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,9 +1,7 @@
 import contextlib
-from pathlib import Path
 import pytest
 import numpy as np
 import seaborn as sns
-import tempfile
 import pandas as pd
 from clm.functions import read_file, read_csv_file, write_to_csv_file
 
@@ -146,91 +144,69 @@ def test_read_file_stream_5_randomize_compressed(dataset_compressed):
     assert data[3] == "CC1(C)C=Cc2c(cc(CO)c(C(=O)O)c2O)O1"  # Line 517
 
 
-def test_write_csv_uncompressed_dataframe():
+def test_write_csv_uncompressed_dataframe(tmp_path):
     iris = sns.load_dataset("iris")
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
-        write_to_csv_file(temp_dir / "iris.csv", iris)
-        df = read_csv_file(temp_dir / "iris.csv")
-        pd.testing.assert_frame_equal(iris, df)
+    write_to_csv_file(tmp_path / "iris.csv", iris)
+    df = read_csv_file(tmp_path / "iris.csv")
+    pd.testing.assert_frame_equal(iris, df)
 
 
-def test_write_csv_compressed_dataframe():
+def test_write_csv_compressed_dataframe(tmp_path):
     iris = sns.load_dataset("iris")
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
-        write_to_csv_file(temp_dir / "iris.csv.gz", iris)
-        df = read_csv_file(temp_dir / "iris.csv.gz")
-        pd.testing.assert_frame_equal(iris, df)
+    write_to_csv_file(tmp_path / "iris.csv.gz", iris)
+    df = read_csv_file(tmp_path / "iris.csv.gz")
+    pd.testing.assert_frame_equal(iris, df)
 
 
-def test_write_csv_uncompressed_iterable():
+def test_write_csv_uncompressed_iterable(tmp_path):
     data = ["foo", "bar", "baz"]
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
-        write_to_csv_file(temp_dir / "data.csv", data, string_format="{}\n")
-        df = read_csv_file(temp_dir / "data.csv", header=None)
-        pd.testing.assert_frame_equal(pd.DataFrame(data), df)
+    write_to_csv_file(tmp_path / "data.csv", data, string_format="{}\n")
+    df = read_csv_file(tmp_path / "data.csv", header=None)
+    pd.testing.assert_frame_equal(pd.DataFrame(data), df)
 
 
-def test_write_csv_compressed_iterable():
+def test_write_csv_compressed_iterable(tmp_path):
     data = ["foo", "bar", "baz"]
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
-        write_to_csv_file(temp_dir / "data.csv.gz", data, string_format="{}\n")
-        df = read_csv_file(temp_dir / "data.csv.gz", header=None)
-        pd.testing.assert_frame_equal(pd.DataFrame(data), df)
+    write_to_csv_file(tmp_path / "data.csv.gz", data, string_format="{}\n")
+    df = read_csv_file(tmp_path / "data.csv.gz", header=None)
+    pd.testing.assert_frame_equal(pd.DataFrame(data), df)
 
 
-def test_write_csv_uncompressed_dataframe_append():
+def test_write_csv_uncompressed_dataframe_append(tmp_path):
     iris = sns.load_dataset("iris")
     iris2 = iris.copy()
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
-        write_to_csv_file(temp_dir / "iris.csv", iris)
-        write_to_csv_file(temp_dir / "iris.csv", iris2, mode="a+")
-        df = read_csv_file(temp_dir / "iris.csv")
-        pd.testing.assert_frame_equal(
-            pd.concat([iris, iris2]).reset_index(drop=True), df
-        )
+    write_to_csv_file(tmp_path / "iris.csv", iris)
+    write_to_csv_file(tmp_path / "iris.csv", iris2, mode="a+")
+    df = read_csv_file(tmp_path / "iris.csv")
+    pd.testing.assert_frame_equal(pd.concat([iris, iris2]).reset_index(drop=True), df)
 
 
-def test_write_csv_compressed_dataframe_append():
+def test_write_csv_compressed_dataframe_append(tmp_path):
     iris = sns.load_dataset("iris")
     iris2 = iris.copy()
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
-        write_to_csv_file(temp_dir / "iris.csv.gz", iris)
-        write_to_csv_file(temp_dir / "iris.csv.gz", iris2, mode="a+")
-        df = read_csv_file(temp_dir / "iris.csv.gz")
-        pd.testing.assert_frame_equal(
-            pd.concat([iris, iris2]).reset_index(drop=True), df
-        )
+    write_to_csv_file(tmp_path / "iris.csv.gz", iris)
+    write_to_csv_file(tmp_path / "iris.csv.gz", iris2, mode="a+")
+    df = read_csv_file(tmp_path / "iris.csv.gz")
+    pd.testing.assert_frame_equal(pd.concat([iris, iris2]).reset_index(drop=True), df)
 
 
-def test_write_csv_uncompressed_iterable_append():
+def test_write_csv_uncompressed_iterable_append(tmp_path):
     data = ["foo", "bar", "baz"]
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
-        write_to_csv_file(temp_dir / "data.csv", data, string_format="{}\n")
-        write_to_csv_file(temp_dir / "data.csv", data, string_format="{}\n", mode="a+")
-        df = read_csv_file(temp_dir / "data.csv", header=None)
-        pd.testing.assert_frame_equal(
-            pd.concat([pd.DataFrame(data), pd.DataFrame(data)]).reset_index(drop=True),
-            df,
-        )
+    write_to_csv_file(tmp_path / "data.csv", data, string_format="{}\n")
+    write_to_csv_file(tmp_path / "data.csv", data, string_format="{}\n", mode="a+")
+    df = read_csv_file(tmp_path / "data.csv", header=None)
+    pd.testing.assert_frame_equal(
+        pd.concat([pd.DataFrame(data), pd.DataFrame(data)]).reset_index(drop=True),
+        df,
+    )
 
 
-def test_write_csv_compressed_iterable_append():
+def test_write_csv_compressed_iterable_append(tmp_path):
     data = ["foo", "bar", "baz"]
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_dir = Path(temp_dir)
-        write_to_csv_file(temp_dir / "data.csv.gz", data, string_format="{}\n")
-        write_to_csv_file(
-            temp_dir / "data.csv.gz", data, string_format="{}\n", mode="a+"
-        )
-        df = read_csv_file(temp_dir / "data.csv.gz", header=None)
-        pd.testing.assert_frame_equal(
-            pd.concat([pd.DataFrame(data), pd.DataFrame(data)]).reset_index(drop=True),
-            df,
-        )
+    write_to_csv_file(tmp_path / "data.csv.gz", data, string_format="{}\n")
+    write_to_csv_file(tmp_path / "data.csv.gz", data, string_format="{}\n", mode="a+")
+    df = read_csv_file(tmp_path / "data.csv.gz", header=None)
+    pd.testing.assert_frame_equal(
+        pd.concat([pd.DataFrame(data), pd.DataFrame(data)]).reset_index(drop=True),
+        df,
+    )
