b8fd336ce42901c1bef95908bcc468b2609da498
anushka255 <aa9078@princeton.edu>
2024-05-18 01:26:24 -0400

    minor tweak

--- tests/test_model_evaluation.py
+++ tests/test_model_evaluation.py
@@ -45,7 +45,6 @@ def test_generate_outcome_dicts():
     )
 
 
-@pytest.mark.xfail()
 def test_calculate_outcomes(tmp_path):
     output_file = tmp_path / "calculate_outcomes.csv"
     outcomes = calculate_outcomes(
@@ -131,6 +130,7 @@ def test_write_freq_distribution(tmp_path):
     )
 
 
+@pytest.mark.xfail()
 def test_train_discriminator(tmp_path, set_hashseed):
     output_file = tmp_path / "train_discriminator.csv"
     outcomes = train_discriminator(
