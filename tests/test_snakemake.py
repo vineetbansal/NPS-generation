94561dde136d20071db053c0954797e1bd7f3d21
Vineet Bansal <vineetbansal@protonmail.com>
2024-05-30 14:51:35 -0400

    precommit checked

--- tests/test_snakemake.py
+++ tests/test_snakemake.py
@@ -42,7 +42,7 @@ def test_snakemake():
         forceall=True,
         verbose=True,
         printshellcmds=True,
-        use_conda=True
+        use_conda=True,
     )
     assert success, "Workflow did not complete successfully"
 
