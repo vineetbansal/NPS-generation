4a908d66ec31e7297d896bfdace7b2dc1ed33ff8
anushka255 <aa9078@princeton.edu>
2024-05-31 13:16:56 -0400

    added seed to write outcome distribution, added test case for it and updated files

--- src/clm/commands/write_outcome_distr.py
+++ src/clm/commands/write_outcome_distr.py
@@ -1,5 +1,11 @@
 import pandas as pd
-from clm.functions import read_file, write_to_csv_file, read_csv_file
+from clm.functions import (
+    read_file,
+    write_to_csv_file,
+    read_csv_file,
+    set_seed,
+    seed_type,
+)
 
 
 def add_args(parser):
@@ -10,16 +16,26 @@ def add_args(parser):
     parser.add_argument(
         "--output_file", type=str, help="Path to the save the output file"
     )
-
+    parser.add_argument(
+        "--seed",
+        type=seed_type,
+        default=None,
+        nargs="?",
+        help="Random seed for reproducibility",
+    )
     return parser
 
 
-def write_outcome_distr(sample_file, max_mols, train_file, pubchem_file, output_file):
+def write_outcome_distr(
+    sample_file, max_mols, train_file, pubchem_file, output_file, seed=None
+):
+    set_seed(seed)
 
-    sample_file = read_csv_file(sample_file, delimiter=",")
-    sample = sample_file.sample(
-        n=max_mols, replace=True, weights=sample_file["size"], ignore_index=True
-    )
+    sample = read_csv_file(sample_file, delimiter=",")
+    if sample.shape[0] > max_mols:
+        sample = sample.sample(
+            n=max_mols, replace=True, weights=sample["size"], ignore_index=True
+        )
 
     pubchem = read_csv_file(
         pubchem_file, delimiter="\t", header=None, names=["smiles", "mass", "formula"]
@@ -35,7 +51,9 @@ def write_outcome_distr(sample_file, max_mols, train_file, pubchem_file, output_
             train.assign(source="train"),
         ]
     )
+
     write_to_csv_file(output_file, combination, columns=["smiles", "source"])
+    return combination[["smiles", "source"]]
 
 
 def main(args):
@@ -45,4 +63,5 @@ def main(args):
         train_file=args.train_file,
         pubchem_file=args.pubchem_file,
         output_file=args.output_file,
+        seed=args.seed,
     )
