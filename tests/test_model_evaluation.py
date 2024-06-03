d3329a65999251eb7b57deb59093530ddad6490e
anushka255 <aa9078@princeton.edu>
2024-06-03 17:05:15 -0400

    minor fix

--- tests/test_model_evaluation.py
+++ tests/test_model_evaluation.py
@@ -10,8 +10,7 @@ from clm.commands.prep_outcomes_freq import prep_outcomes_freq
 from clm.commands.write_nn_Tc import prep_nn_tc, write_nn_Tc
 from clm.commands.train_discriminator import train_discriminator
 from clm.commands.write_freq_distribution import write_freq_distribution
-from clm.commands.calculate_outcome_distrs import calculate_outcome_distr
-from clm.commands.calculate_outcome_distrs import write_outcome_distr
+from clm.commands.calculate_outcome_distrs import calculate_outcome_distr, write_outcome_distr
 from clm.commands.add_carbon import add_carbon
 from clm.commands.plot import plot
 from clm.functions import assert_checksum_equals, read_csv_file, local_seed
@@ -184,7 +183,6 @@ def test_train_discriminator(tmp_path):
 
 
 def test_write_outcome_distr(tmp_path):
-    output_file = tmp_path / "write_outcome_distr.csv"
     outcomes = write_outcome_distr(
         sample_file=test_dir
         / "snakemake_output/0/prior/samples/LOTUS_truncated_SMILES_0_unique_masses.csv",
@@ -192,8 +190,6 @@ def test_write_outcome_distr(tmp_path):
         train_file=test_dir
         / "snakemake_output/0/prior/inputs/train_LOTUS_truncated_SMILES_0.smi",
         pubchem_file=test_dir / "PubChem_truncated.tsv",
-        output_file=output_file,
-        seed=0,
     )
 
     true_outcomes = read_csv_file(test_dir / "write_outcome_distr.csv")
