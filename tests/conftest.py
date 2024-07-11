0cab7196d7132f94dcc20017e56dbef321205889
Vineet Bansal <vineetbansal@protonmail.com>
2024-07-11 11:59:36 -0400

    setting seed in a fixture at the function level

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import gzip
 import pytest
+from clm.functions import set_seed
 
 base_dir = Path(__file__).parent.parent
 dataset_path = base_dir / "tests/test_data/LOTUS_truncated.txt"
@@ -20,3 +21,12 @@ def dataset_compressed():
         with open(dataset_path, "rb") as f:
             g.write(f.read())
     yield path
+
+
+@pytest.fixture(scope="function", autouse=True)
+def seed():
+    """
+    Set the random seed to 0 for all tests.
+    This prevents individual tests that use set_seed from affecting each other.
+    """
+    set_seed(0)
