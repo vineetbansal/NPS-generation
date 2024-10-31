e8f82996d17be79e1763db8d3e19299d1927da34
Vineet Bansal <vineetbansal@protonmail.com>
2024-10-31 14:53:15 -0400

    some bugs fixed when we have no valid smiles; using correct heldout file for a fold

--- src/clm/__init__.py
+++ src/clm/__init__.py
@@ -1,10 +1,5 @@
 import logging.config
 
-# TODO PR249 : pandas is being obnoxious
-import warnings
-
-warnings.simplefilter(action="ignore", category=FutureWarning)
-
 # The _version.py file is managed by setuptools-scm
 #   and is not in version control.
 try:
